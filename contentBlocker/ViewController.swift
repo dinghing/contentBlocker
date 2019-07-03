e38f8fc8531dbe8c0a4800c34cb7671b2c969011
dinghing <dinghing154@gmail.com>
2019-07-03 11:11:11 +0900

    Initial Commit

---
+++ contentBlocker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contentBlocker
+//
+//  Created by ding qi on 2019/07/03.
+//  Copyright © 2019 ding qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ contentBlockerTests/contentBlockerTests.swift
@@ -0,0 +1,34 @@
+//
+//  contentBlockerTests.swift
+//  contentBlockerTests
+//
+//  Created by ding qi on 2019/07/03.
+//  Copyright © 2019 ding qi. All rights reserved.
+//
+
+import XCTest
+@testable import contentBlocker
+
+class contentBlockerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
